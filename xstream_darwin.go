37ff7421ee8b9b28bfccdc4ddf19e86503f143b1
memclr <memclr@memclrdeMacBook-Pro.local>
2021-09-21 15:30:22 +0800

    feat: update porting

--- xstream_darwin.go
+++ xstream_darwin.go
@@ -1,5 +1,3 @@
-// +build darwin linux
-
 package goxstream
 
 // #cgo CFLAGS: -I./include -fPIC
--- xstream_linux.go
+++ xstream_linux.go
@@ -1,7 +1,7 @@
 package goxstream
 
 // #cgo CFLAGS: -I./include -fPIC
-// #cgo LDFLAGS: -loci
+// #cgo LDFLAGS: -lclntsh
 /* #
 #include "xstrm.c"
 */
